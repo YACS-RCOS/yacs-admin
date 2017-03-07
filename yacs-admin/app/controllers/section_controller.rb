88aac4c1f544a418e47bc4b9fc68d4af64bfdf99
kburk1997 <kburk97@optonline.net>
2017-03-07 13:35:52 -0500

    add section view

--- yacs-admin/app/controllers/section_controller.rb
+++ yacs-admin/app/controllers/section_controller.rb
@@ -1,9 +1,19 @@
 class SectionController < ApplicationController
+  include SectionHelper
+  helper :all
   def index
     @sections=Section.all
   end
 
   def show
+    section_id=params[:id]
+    search_params={:id => section_id, :show_periods =>''}
+    #puts course_id
+    #use .elements[0], since .find returns a collection
+    @section=Section.find(:all, :params=>search_params).elements[0]
 
+    #find course
+    course_params={:id=>@section.course_id}
+    @course=Course.find(:all,:params=>course_params).elements[0]
   end
 end
--- yacs-admin/app/helpers/section_helper.rb
+++ yacs-admin/app/helpers/section_helper.rb
@@ -1,2 +1,36 @@
 module SectionHelper
+
+  def day_to_string(day)
+    case day
+    when 0
+      return "Sunday"
+    when 1
+      return "Monday"
+    when 2
+      return "Tuesday"
+    when 3
+      return "Wednesday"
+    when 4
+      return "Thursday"
+    when 5
+      return "Friday"
+    when 6
+      return "Saturday"
+    end
+  end
+
+  def format_time(time)
+    time_string=time.to_s
+    hour=time_string[0,2]
+    minute=time_string[2,2]
+    suffix="AM"
+    if hour.to_i > 12
+      hour=((hour.to_i)-12).to_s
+      suffix="PM"
+    elsif hour.to_i == 12
+      suffix="PM"
+    end
+
+    return hour+":"+minute+" "+suffix
+  end
 end
