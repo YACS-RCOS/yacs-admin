559f0f7285ebe854f4c3d75b733d9ddd69c0d4bd
kburk1997 <kburk97@optonline.net>
2017-10-12 14:34:45 -0400

    create blank navbar component

--- yacs-admin-angular/src/app/app.module.ts
+++ yacs-admin-angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppNavComponent
   ],
   imports: [
     BrowserModule
