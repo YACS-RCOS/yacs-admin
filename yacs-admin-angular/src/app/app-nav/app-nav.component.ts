9ae5fe88154c84be0bbcb30dd416f26cd8b47268
kburk1997 <kburk97@optonline.net>
2017-10-12 15:48:19 -0400

    add navbar

--- yacs-admin-angular/src/app/app-nav/app-nav.component.ts
+++ yacs-admin-angular/src/app/app-nav/app-nav.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-app-nav',
+  selector: 'app-nav',
   templateUrl: './app-nav.component.html',
   styleUrls: ['./app-nav.component.css']
 })
