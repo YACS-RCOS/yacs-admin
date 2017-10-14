84e40647d6efeb177c4f5f57038a611e654825d7
kburk1997 <kburk97@optonline.net>
2017-10-14 15:26:40 -0400

    create welcome page component

--- yacs-admin-angular/src/app/app.module.ts
+++ yacs-admin-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppNavComponent } from './app-nav/app-nav.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNavComponent
+    AppNavComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule
