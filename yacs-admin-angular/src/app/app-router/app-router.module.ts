1593c934f3728735a6d3b3ee3dd502281ba1f18f
kburk1997 <kburk97@optonline.net>
2017-10-17 17:27:09 -0400

    added one thing, broke another

--- yacs-admin-angular/src/app/app-router/app-router.module.ts
+++ yacs-admin-angular/src/app/app-router/app-router.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from '../welcome-page/welcome-page.component';
-
+import { SchoolListComponent } from '../school/school-list/school-list.component';
 const routes: Routes = [
-  {path: '', component: WelcomePageComponent}
+  {path: '', component: WelcomePageComponent},
+  {path: '/schools', component: SchoolListComponent}
 ];
 
 @NgModule({
