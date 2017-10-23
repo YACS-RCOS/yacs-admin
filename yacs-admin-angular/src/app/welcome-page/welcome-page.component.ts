5f2a270b198c7b64750b638f4b45525d8a94132f
Cole Gregory <cag171@me.com>
2017-10-22 22:43:14 -0400

    Fixed images

--- yacs-admin-angular/src/app/welcome-page/welcome-page.component.ts
+++ yacs-admin-angular/src/app/welcome-page/welcome-page.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-page.component.css']
 })
 export class WelcomePageComponent implements OnInit {
-  private indexHeader = require("../../assets/images/index_header.png");
-
   constructor() { }
 
   ngOnInit() {
