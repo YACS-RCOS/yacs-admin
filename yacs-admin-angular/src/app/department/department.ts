ef3dc094a52ca430450717d93bb1e8ca1167b707
Cole Gregory <cag171@me.com>
2017-10-24 18:33:57 -0400

    Added Department

---
+++ yacs-admin-angular/src/app/department/department.ts
@@ -0,0 +1,9 @@
+/* Department class
+ * Based off of: https://github.com/YACS-RCOS/yacs-web/blob/master/src/app/school-list/department/department.ts
+ */
+
+export class Department {
+  id: number;
+  code: number;
+  name: string;
+}
---
+++ yacs-admin-angular/src/app/school-list/school.ts
@@ -0,0 +1,11 @@
+/* School class
+ * Based off of: https://github.com/YACS-RCOS/yacs-web/blob/master/src/app/school-list/school.ts
+ */
+
+export class School {
+  id: number;
+  name: string;
+  departments: string[]; //Let this be a string array for now,
+                         // we can change this to a department array later
+}
+
