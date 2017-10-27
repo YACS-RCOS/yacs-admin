0847546de57272efe21fb1f97ae0a43b41c6fa06
kburk1997 <kburk97@optonline.net>
2017-10-27 15:24:43 -0400

    Add department array to schools

--- yacs-admin-angular/src/app/school/school.spec.ts
+++ yacs-admin-angular/src/app/school/school.spec.ts
@@ -1,19 +1,20 @@
 import { School } from './school';
-
+import { Department } from '../department/department'
 describe('School', () => {
   it('has name', () => {
-    const school = new School(1, 'School of StackOverflow');
+    const school = new School(1, 'School of StackOverflow',[]);
     expect(school.name).toBe('School of StackOverflow');
   });
 
   it('has id',() => {
-    const school = new School(1, 'School of StackOverflow');
+    const school = new School(1, 'School of StackOverflow',[]);
     expect(school.id).toBe(1);
   });
 
-  /*it('has departments', () => {
-    const school = new School(1, 'School of StackOverflow',[]);
-    expect(school.departments).toBeDefined();
-  });*/
+  it('has departments', () => {
+    const dept1 = new Department(1,'CPYP','Copying and Pasting',1);
+    const school = new School(1, 'School of StackOverflow',[dept1]);
+    expect(school.departments[0].name).toBe('Copying and Pasting');
+  });
 
 });
