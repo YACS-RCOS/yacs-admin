fee8228f87e145d7ef95f62b51ea99364af94bf2
kburk1997 <kburk97@optonline.net>
2017-10-31 18:48:17 -0400

    fix isue 100, test later

--- yacs-admin-angular/src/app/department/department-list/department-list.component.ts
+++ yacs-admin-angular/src/app/department/department-list/department-list.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Department } from '../department';
+
+const DEPTS: Department[] = [
+  {id: 1, code: 'ASTR', name: 'Astronomy', school_id: 3},
+  {id: 2, code: 'BCBP', name: 'Biochemistry and Biophysics', school_id: 3},
+  {id: 3, code: 'BIOL', name: 'Biology', school_id: 3},
+  {id: 4, code: 'CHEM', name: 'Chemistry', school_id: 3},
+];
+
 @Component({
   selector: 'department-list',
   templateUrl: './department-list.component.html',
   styleUrls: ['./department-list.component.css']
 })
-export class DepartmentListComponent implements OnInit {
 
+
+
+export class DepartmentListComponent implements OnInit {
+  departments = DEPTS;
   constructor() { }
 
   ngOnInit() {
