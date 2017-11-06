424c4e21063a78a254f59be5bda791143b19581a
Cole Gregory <cag171@me.com>
2017-11-05 20:28:55 -0500

    Updated tests

--- yacs-admin-angular/src/app/app.component.ts
+++ yacs-admin-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'YACS Admin';
+  constructor(private router: Router, private titleService: Title) {}
 }
--- yacs-admin-angular/src/app/department/department-list/department-list.component.spec.ts
+++ yacs-admin-angular/src/app/department/department-list/department-list.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DepartmentListComponent } from './department-list.component';
 
-describe('SchoolListComponent', () => {
+describe('DepartmentListComponent', () => {
   let component: DepartmentListComponent;
   let fixture: ComponentFixture<DepartmentListComponent>;
 
@@ -30,23 +30,24 @@ describe('SchoolListComponent', () => {
     expect(ths[1].textContent).toContain('Code');
     expect(ths[2].textContent).toContain('Name');
   });
+
   it('renders deparment', () => {
     //making sure we can access component departments
     console.log(component.departments);
 
-        
     //write the actual tests here
-  
-    var tbody=document.getElementsByTagName("tbody");
+    var tbody = document.getElementsByTagName("tbody");
     var rows = tbody[0].getElementsByTagName('tr');
     console.log(tbody[0].getElementsByTagName('tr'));
-    //dummy test
-    
+
     for (var i = 0; i<component.departments.length; i++){
+      console.log("DEPARTMENT COMPONENT");
       console.log(component.departments[i]);
+      console.log("ROW");
       console.log(rows[i]);
       //What we want to do is check each department properties
       //Make sure they correspond with rows of same index
+
     }
     expect(1).toEqual(1);
 
