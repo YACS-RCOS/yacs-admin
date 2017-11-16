441b2197c77c7a62e1be81c2f758eba69114fc5a
kburk1997 <kburk97@optonline.net>
2017-11-16 11:06:57 -0500

    add credit range, fix course data, add more links to detail view

--- yacs-admin-angular/src/app/course/course-list/course-list.component.ts
+++ yacs-admin-angular/src/app/course/course-list/course-list.component.ts
@@ -11,7 +11,16 @@ export class CourseListComponent implements OnInit {
   courses = COURSES;
   departments = DEPTS;
   schools = SCHOOLS;
-  constructor() { }
+  constructor() {}
+  
+  /*Modified from yacs-web, "credit(s)" will 
+   * not display because credits is column title*/
+  public creditRange(course: Course): string{
+    if(course.min_credits!=course.max_credits){
+      return String(course.min_credits)+'-'+String(course.max_credits);
+    }
+    return String(course.min_credits);
+  }
   selectedCourse: Course;
   onSelect(course: Course): void{
     this.selectedCourse=course;
--- yacs-admin-angular/src/app/mock-data.ts
+++ yacs-admin-angular/src/app/mock-data.ts
@@ -9,9 +9,9 @@ export const COURSES: Course[] = [
       description: 'Check back soon'},
     {id: 2, name: 'Special Projects in Astronomy', num: '2940', department_code: 'ASTR',
       department_id: 1, min_credits: 1, max_credits: 6,
-      description:'Study and research in various fields of astronomy to demonstrate \
-      interest in and ability for independent work.Prerequisites/Corequisites: Prerequisite:\
-       permission of instructor.When Offered: Fall and spring terms annually.Credit Hours: 3'},
+      description:`Study and research in various fields of astronomy to demonstrate \
+       interest in and ability for independent work.Prerequisites/Corequisites: Prerequisite:\
+       permission of instructor.When Offered: Fall and spring terms annually.Credit Hours: 3`},
      {id: 3, name: 'Particle Astrophysics', num: '2961', department_code: 'ASTR',
        department_id: 1, min_credits: 4, max_credits: 4,
        description: 'Description not available...'},
