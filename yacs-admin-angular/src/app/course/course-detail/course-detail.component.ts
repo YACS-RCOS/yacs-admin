e34ce0873be91c989c72fc4d8be57cb0265686bf
kburk1997 <kburk97@optonline.net>
2018-02-08 17:38:14 -0500

    finish course detail view

--- yacs-admin-angular/src/app/course/course-detail/course-detail.component.ts
+++ yacs-admin-angular/src/app/course/course-detail/course-detail.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Course} from '../course';
+import {Department} from '../../department/department';
+import {YacsService} from '../../services/yacs.service';
+import {ActivatedRoute, Router} from '@angular/router';
 @Component({
   selector: 'app-course-detail',
   templateUrl: './course-detail.component.html',
   styleUrls: ['./course-detail.component.css']
 })
 export class CourseDetailComponent implements OnInit {
-
-  constructor() { }
+  @Input() course: Course;
+  depts: Department[];
+  constructor(private yacsService: YacsService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+     
+    this.getCourse();
+    this.getDepts();
+  }
+
+  getDepts(){
+    this.yacsService.getDepts()
+      .subscribe(depts => this.depts = depts);
+  }
+
+  getCourse(){
+    let id: number;
+    this.route.params.subscribe(params=>{id=+params['id']});
+
+    this.yacsService.getCourseByID(id)
+      .subscribe(course => this.course = course);
   }
 
 }
