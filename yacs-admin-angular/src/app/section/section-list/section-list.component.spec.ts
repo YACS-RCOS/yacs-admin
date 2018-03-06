d5a2af3a2a6a1094fef82a91702d841632171ce9
kburk1997 <kburk97@optonline.net>
2018-03-06 14:01:14 -0500

    add tests for passing in a section

--- yacs-admin-angular/src/app/section/section-list/section-list.component.spec.ts
+++ yacs-admin-angular/src/app/section/section-list/section-list.component.spec.ts
@@ -77,7 +77,74 @@ describe('SectionListComponent', ()=>{
 
   });
 
+    describe('valid course id queried', ()=>{
 
+
+    let component: SectionListComponent;
+    let fixture: ComponentFixture<SectionListComponent>;
+    let mockParams = [{'course_id':1}];
+    beforeEach(async(() => {
+      TestBed.configureTestingModule({
+        imports: [RouterTestingModule, HttpClientModule, HttpClientInMemoryWebApiModule.forRoot(    InMemoryDataService, {dataEncapsulation: false, passThruUnknownUrl: true, delay: 100} )],
+        declarations: [ SectionListComponent ],
+        providers: [
+          {provide: YacsService, useClass: FakeYacsService}, 
+          {provide: ActivatedRoute, useValue: {'queryParams': Observable.from(mockParams)}}]
+      })
+      .compileComponents();
+    }));
+
+     beforeEach(() => {
+      fixture = TestBed.createComponent(SectionListComponent);
+      component = fixture.componentInstance;
+      spyOn(component, 'getSections');
+
+      spyOn(component, 'getSelectedCourse');
+      spyOn(component, 'setCourseId');
+      spyOn(component, 'getCourseSections');
+      fixture.detectChanges();
+
+      fixture.whenStable().then(()=>{
+        fixture.detectChanges();
+      });
+       
+    });
+
+
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should call getSections',()=>{
+      expect(component.getSections).toHaveBeenCalled();
+    });
+
+    it('should call setCourseId', ()=>{
+      expect(component.setCourseId).toHaveBeenCalled();
+    });
+
+    it('should  call getCourseSections', async()=>{
+      tick(100);
+      expect(component.getCourseSections).toHaveBeenCalled();
+    });
+
+    it('should call getSelectedCourse', async()=>{
+      tick(100);
+      expect(component.getSelectedCourse).toHaveBeenCalled();
+    });
+
+    it('should not display a header called \"All Sections\"', async()=>{
+      tick();
+      var header=document.getElementsByTagName('h2')[0];
+      expect(header.textContent).not.toMatch('All Sections');
+    });
+
+    it('should display the table',() => {
+      expect(document.getElementsByClassName('table')).toBeTruthy();
+    });
+
+  });
 
 
 
